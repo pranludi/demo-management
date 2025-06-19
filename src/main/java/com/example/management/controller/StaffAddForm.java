9a3b4504120198304f34f442f7fc15bd3d397116
daekwonpark <pranludi@gmail.com>
2025-06-19 08:52:33 +0700

    controller - add staff

---
+++ src/main/java/com/example/management/controller/StaffAddForm.java
@@ -0,0 +1,51 @@
+package com.example.management.controller;
+
+import java.time.LocalDate;
+
+public class StaffAddForm {
+    private String loginId;
+    private String password;
+    private String name;
+    private LocalDate birthDate;
+    private String email;
+
+    public String getLoginId() {
+        return loginId;
+    }
+
+    public void setLoginId(String loginId) {
+        this.loginId = loginId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/main/java/com/example/management/controller/StaffController.java
@@ -0,0 +1,41 @@
+package com.example.management.controller;
+
+import com.example.management.domain.Staff;
+import com.example.management.service.StaffService;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class StaffController {
+
+    private final StaffService staffService;
+
+    public StaffController(StaffService staffService) {
+        this.staffService = staffService;
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/add/form")
+    public String staffAddForm() {
+        return "staffAddForm";
+    }
+
+    @PostMapping("/add/form")
+    public String staffAddForm(StaffAddForm staffAddForm) {
+        Staff staff = new Staff();
+        staff.setLoginId(staffAddForm.getLoginId());
+        staff.setPassword(staffAddForm.getPassword());
+        staff.setName(staffAddForm.getName());
+        staff.setBirthDate(staffAddForm.getBirthDate());
+        staff.setEmail(staffAddForm.getEmail());
+        staffService.addStaff(staff);
+        return "redirect:/list";
+    }
+
+}
